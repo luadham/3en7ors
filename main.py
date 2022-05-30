74159fcacd4ead778ce0da1cef35b82a7f456615
luadham <luadham@gmail.com>
2022-05-30 18:08:15 +0200

    GUI

---
+++ main.py
@@ -0,0 +1,11 @@
+from _tkinter import *
+from tkinter import *
+
+# This is the root Window
+root = Tk()
+# Title of Window
+root.title("3en7ors")
+# Size of Window
+root.geometry("500x500")
+# INIT Window
+root.mainloop()
