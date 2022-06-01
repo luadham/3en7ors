a173f8a1c9f27115b2f4834d77558c957e2b4a97
Marwan Moussa <maromoussa88@gmail.com>
2022-06-01 20:43:31 +0200

    add thread

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from PortScanner import PortScanner as pscanner
 from VulnerabilityScanner import VulnerabilityScanner as vscanner
 import tkinter as tk
 import tkinter.scrolledtext as st
+import customtkinter
+import threading
+import time
+
 
 main_color = "#414141"
 
@@ -12,44 +16,45 @@ main_color = "#414141"
 root = Tk()
 # create Label
 
-open_tcp_ports = []
-open_udp_ports = []
+open_tcp_ports = []  # first
+open_udp_ports = []  # kol port udp osado el service bet3to
 udp_services = []
 
-open_tcp_vul_ports = []
+open_tcp_vul_ports = []  # kol port wel banner bet3to
 open_tcp_vul_banners = []
 
 bg = PhotoImage(file="res/img.png")
-new_back = Label(root,image=bg)
-new_back.place(x=0,y=0,relwidth=1,relheight=1)
+new_back = Label(root, image=bg)
+new_back.place(x=0, y=0, relwidth=1, relheight=1)
 
 text_area = st.ScrolledText(root,
-                            width = 50,
-                            height = 19,
-                            font = ("Times New Roman",
-                                    15))
-text_area.grid(column = 0, pady = 80, padx = 80)
+                            width=50,
+                            height=19,
+                            font=("Times New Roman",
+                                  15))
+text_area.grid(column=0, pady=80, padx=80)
 
 text_area.insert(tk.INSERT,
                  """\
 1st
-                 """,'x')
+                 """, 'x')
 
 
 text_area.insert(tk.INSERT,
                  """\
                  2nd
                  
-                 """,'y')
+                 """, 'y')
 text_area.tag_config('x', foreground='red')
 text_area.tag_config('y', foreground='gold')
 
 
-
-text_area.configure(state ='disabled')
+text_area.configure(state='disabled')
 # This Function Take victim ip and number if ports that we want to scan
 # and then fill open_tcp_ports, open_udp_ports, udp_services
 # You Should Call it when user click to Scan Button
+
+
 def scan_victim_ports(victim, number_of_ports):
     port_scanner = pscanner(victim=victim, number_of_ports=number_of_ports)
 
@@ -83,7 +88,9 @@ def scan_victim_vul(victim, number_of_ports):
 # This is Function Will invoke when user click on scan button
 # TODO: WE MUST CALL THIS FUNCTION IN ANOTHER THREAD
 # Print statement just for illustration YOU Should Delete it
-def scan_victim():
+
+
+def scan_victim2():
     victim = target_input.get()
     number_of_ports = number_of_ports_input.get()
     if (scan_victim_ports(victim=victim, number_of_ports=number_of_ports) and scan_victim_vul(victim=victim, number_of_ports=number_of_ports)):
@@ -105,6 +112,13 @@ def scan_victim():
     else:
         assert "Check Victim IP of Number of Ports"
 
+    print(len(open_tcp_ports))
+
+
+def runDiffThread():
+    t = threading.Thread(target=scan_victim2)
+    t.start()
+
 
 target_label = Label(root, text="Target", bg=main_color, fg="white")
 target_label.place(x=10, y=13)
@@ -119,7 +133,7 @@ number_of_ports_input = Entry(root)
 number_of_ports_input.insert(0, '# of Ports')
 number_of_ports_input.place(x=250, y=13)
 
-scan_button = Button(root, text="Scan Port", command=scan_victim)
+scan_button = Button(root, text="Scan Port", command=runDiffThread)
 scan_button.place(x=400, y=10)
 
 #vul_text_area = Text(root)
@@ -152,7 +166,6 @@ scan_button.place(x=400, y=10)
 #          background = 'green',
 #          foreground = "white").grid(column = 0,
 #                                     row = 0)
-
 
 
 root['bg'] = main_color
